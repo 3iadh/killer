5bd892be6a7687c4a91ba24457a4793ae1226749
Iadh Bouoni <iadh.bouoni@capgemini.com>
2018-03-19 11:29:09 +0100

    Correction ano accueil

--- ihm_killer/src/app/containers/accueil.component.ts
+++ ihm_killer/src/app/containers/accueil.component.ts
@@ -1,37 +1,35 @@
-import { Component, OnInit, Input, ViewChild, AfterViewInit,HostListener } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, AfterViewInit, ElementRef, HostListener  } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { ContractService } from '../services/contracts.service';
-import { AccueilService } from '../services/accueil.service';
+import { ContractService } from "../services/contracts.service";
+import { AccueilService } from "../services/accueil.service";
 import { MdTooltip } from '@angular/material';
 import { ToastyService, ToastyConfig, ToastOptions, ToastData } from 'ng2-toasty';
 
-
 @Component({
-  selector: 'app-accueil-root',
+  selector: 'accueil-root',
   templateUrl: '../templates/accueil.component.html',
-  styleUrls: ['../styles/accueil.component.css'],
+  styleUrls: ['../styles/accueil.component.css']
 })
 export class AccueilComponent implements OnInit {
   isConnected: boolean;
-   message: any;
   contract = [];
   contractDetail = null;
-  cardClass = '';
   contratId;
+  showButtons = false;
+  loadDone = false;
 
-  // Dans le cas ou j'ai tué quelqu'un et il faut que je déclare l'avoir tué
+  //Dans le cas ou j'ai tué quelqu'un et il faut que je déclare l'avoir tué
   killerValidationSet = false;
   canSetKillerValidation = false;
 
-  // Dans le cas ou quelqu'un m'a tué et il faut que je confirme
+  //Dans le cas ou quelqu'un m'a tué et il faut que je confirme
   imKilled = false;
   canSetImKilled = false;
-  missionStatus = false;
 
   infoDetail = {
     'targetInfo': {
-      'bench': 'Espoir',
+      'bench': '',
       'firstname': '',
       'killerlastname': '',
       'killerusername': '',
@@ -42,7 +40,14 @@ export class AccueilComponent implements OnInit {
     'action': '',
     'startDate': '',
     'id': null,
-    'canSetKillerValidation': 0
+    'canSetKillerValidation': 0,
+    'userInfo': {
+      'name': '',
+      'username': '',
+      'inscriptionDate': null,
+      'email': '',
+      'img': null
+    }
   };
   deathDetail = {
     'killerInfo': {
@@ -60,11 +65,12 @@ export class AccueilComponent implements OnInit {
     'id': null,
     'killed': false
   };
+  killedUsers = [];
+  killedUsersJson = [];
 
-  // @Input('parentData') incomingData: string;
-  //@Input() incomingData: string;
-  afficheDetail: boolean;
-  myUserId;
+  @Input('parentData') incomingData: string;
+
+  myUserId
   userId;
 
   constructor(private translate: TranslateService,
@@ -73,7 +79,7 @@ export class AccueilComponent implements OnInit {
     private contractservice: ContractService,
     private toastyService: ToastyService,
     private toastyConfig: ToastyConfig,
-    private AccueilService: AccueilService
+    private AccueilService: AccueilService,
   ) {
     translate.setDefaultLang('fr');
     this.toastyConfig.theme = 'material';
@@ -89,7 +95,6 @@ export class AccueilComponent implements OnInit {
     }
 
   }
-
   canShowMenu = true;
   md = false;
   killDetails = {
@@ -97,7 +102,6 @@ export class AccueilComponent implements OnInit {
     'date': '',
     'time': ''
   }
-  
   public innerWidth: any
   ngOnInit() {
     this.innerWidth = window.innerWidth;
@@ -105,7 +109,6 @@ export class AccueilComponent implements OnInit {
       this.canShowMenu = false;
     }
   }
-
   @HostListener('window:resize', ['$event'])
   onResize(event) {
     this.innerWidth = window.innerWidth;
@@ -116,8 +119,7 @@ export class AccueilComponent implements OnInit {
     }
   }
 
-
-  // permet de confirmer que nous avons tué le target
+  //permet de confirmer que nous avons tué le target
   setContratRempli() {
     let id = this.infoDetail.id;
     if (this.infoDetail.id != null) {
@@ -125,14 +127,15 @@ export class AccueilComponent implements OnInit {
         this.killerValidationSet = true;
         // this.canSetKillerValidation = false;
 
-        // Recharger la page après validation
+        //Recharger la page après validation
         this.reloadData(this.userId);
 
-      });
+      })
     }
   }
+ 
+  //Permet en tant que joueur de dire que j'ai été quillé
 
-  // Permet en tant que joueur de dire que j'ai été killé
   setImKilled() {
     let id = this.deathDetail.id;
     if (id != null) {
@@ -168,56 +171,55 @@ export class AccueilComponent implements OnInit {
       console.log(res);
     });
   }
-  // recharger les données
+  //recharger les données
 
   reloadData(userId) {
     this.contractservice.getMyContract(userId).then(res => {
       this.isConnected = true;
-       this.message = userId;
-      if (res.data.length !== 0) {
-        this.contract = res.data;
-        this.contractDetail = this.contract[0].contractid.data;
-        let toolUrl = this.AccueilService.getMAinUrl() + this.contractDetail.contracttoolid.data.toolid.data.toolimage.data.url;
-        let toolName = this.contractDetail.contracttoolid.data.toolid.data.toolname;
-        let targetImg = this.AccueilService.getMAinUrl() + this.contractDetail.contacttargetid.data.killerphoto.data.url;
-        console.log(this.contractDetail);
-        this.infoDetail = {
-          'targetInfo': {
-            'bench': this.contractDetail.contacttargetid.data.killerbench,
-            'firstname': this.contractDetail.contacttargetid.data.killerfirstname,
-            'killerlastname': this.contractDetail.contacttargetid.data.killerlastname,
-            'killerusername': this.contractDetail.contacttargetid.data.killersurname,
-            'userImg': targetImg,
-          },
-          'tool': toolUrl,
-          'toolName': toolName,
-          'action': this.contractDetail.contracttoolid.data.toolusage,
-          'startDate': this.contractDetail.contractstartdate,
-          'id': this.contract[0].id,
-          'canSetKillerValidation': this.contract[0].killervalidation
-        };
-
-
-      if (this.cardClass === '') {
-        this.cardClass = 'card';
+      this.contract = res.data;
+      this.contractDetail = this.contract[0].contractid.data;
+      let toolUrl = this.AccueilService.getMAinUrl() + this.contractDetail.contracttoolid.data.toolid.data.toolimage.data.url;
+      let toolName = this.contractDetail.contracttoolid.data.toolid.data.toolname;
+      let targetImg = this.AccueilService.getMAinUrl() + this.contractDetail.contacttargetid.data.killerphoto.data.url;
+      let userImg = this.AccueilService.getMAinUrl() + this.contract[0].killerid.data.killerphoto.data.url;
+      let inscriptionDate = new Date(this.contract[0].killerid.data.inscriptiondate);
+      let options = { day: 'numeric', month: 'numeric', year: 'numeric' }
+      this.infoDetail = {
+        'targetInfo': {
+          'bench': this.contractDetail.contacttargetid.data.killerbench,
+          'firstname': this.contractDetail.contacttargetid.data.killerfirstname,
+          'killerlastname': this.contractDetail.contacttargetid.data.killerlastname,
+          'killerusername': '@' + this.contractDetail.contacttargetid.data.killersurname,
+          'userImg': targetImg,
+        },
+        'tool': toolUrl,
+        'toolName': toolName,
+        'action': this.contractDetail.contracttoolid.data.toolusage,
+        'startDate': this.contractDetail.contractstartdate,
+        'id': this.contract[0].id,
+        'canSetKillerValidation': this.contract[0].killervalidation,
+        'userInfo': {
+          'name': this.contract[0].killerid.data.killerfirstname + " " + this.contract[0].killerid.data.killerlastname,
+          'username': '@' + this.contract[0].killerid.data.killersurname,
+          'inscriptionDate': inscriptionDate.toLocaleString('fr-FR', options),
+          'email': this.contract[0].killerid.data.killermail,
+          'img': userImg
+        }
       }
-    } else {
-      this.missionStatus = true;
-    }
+      this.getUsersKilled(userId);
     }).catch(res => {
       console.log(res);
     });
   }
 
 
-  // Récupérer le contrat que j'ai perdu
+  //Récupérer le contrat que j'ai perdu
 
   getLostedContrat(id) {
     this.contractservice.getRequestConfirmation(id).then(res => {
       let contractId;
       if (res.data[0] != null) {
         contractId = res.data[0].id;
-        console.log(res.data);
         if (contractId !== undefined) {
           this.contractservice.getUnConfirmedContract(contractId).then(res => {
             this.contract = res.data;
@@ -241,8 +243,7 @@ export class AccueilComponent implements OnInit {
                 'startDate': this.contractDetail.contractstartdate,
                 'id': this.contract[0].id,
                 'killed': true
-              };
-              this.cardClass = 'cardLost';
+              }
             }
             this.reloadData(this.userId);
           });
@@ -250,34 +251,63 @@ export class AccueilComponent implements OnInit {
         else {
           return null;
         }
-      } else {
+      }
+      else {
 
         this.reloadData(this.userId);
       }
-    });
+    })
   }
   getUsersKilled(userId) {
     this.contractservice.getUsersKilled(userId).then(res => {
+      this.killedUsersJson = res.data;
+      this.fillKilledUsers(res.data);
       console.log(res);
     }).catch(res => {
       console.log(res)
     });
 
   }
+
+  fillKilledUsers(killedUsersJson) {
+    for (var i = 0; i < killedUsersJson.length; i++) {
+      let endDate = new Date(killedUsersJson[i].handlingenddate);
+      let options = { day: 'numeric', month: 'numeric', year: 'numeric' };
+      let killedUser = {
+        'contractDetails': {
+          'endDate': endDate.toLocaleString('fr-FR', options),
+          'tool': killedUsersJson[i].contractid.data.contracttoolid.data.toolid.data.toolname
+        },
+        'name': killedUsersJson[i].contractid.data.contacttargetid.data.killerfirstname + " " + killedUsersJson[i].contractid.data.contacttargetid.data.killerlastname,
+        'username': '@' + killedUsersJson[i].contractid.data.contacttargetid.data.killersurname,
+        'bench': killedUsersJson[i].contractid.data.contacttargetid.data.killerbench,
+        'userImg': this.AccueilService.getMAinUrl() + killedUsersJson[i].contractid.data.contacttargetid.data.killerphoto.data.url
+      }
+      this.killedUsers.push(killedUser);
+    }
+    this.loadDone = true;
+
+  }
   addToast() {
     var toastOptions: ToastOptions = {
-      title: 'Nice',
-      msg: 'Vous venez de killer XXX',
+      title: "Nice",
+      msg: "Vous venez de killer XXX",
       showClose: true,
       timeout: 5000,
       theme: 'default'
     };
-
-    // Add see all possible types in one shot
+    // Add see all possible types in one shot 
     this.toastyService.info(toastOptions);
     // this.toastyService.success(toastOptions);
     // this.toastyService.wait(toastOptions);
     // this.toastyService.error(toastOptions);
     // this.toastyService.warning(toastOptions);
   }
+  confirmMouseHoverLeave() {
+    this.showButtons = false;
+  }
+  confirmMouseHoverEnter() {
+    this.showButtons = true;
+  }
+
 }
