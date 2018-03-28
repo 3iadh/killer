bf373cb51f93d796a33a0aa91bbea70562657104
Iadh Bouoni <iadh.bouoni@capgemini.com>
2018-03-28 10:34:47 +0200

    update conf

--- ihm_killer/src/app/config/api.ts
+++ ihm_killer/src/app/config/api.ts
@@ -1,16 +1,16 @@
 
 //Paramètres serveur sur lequel est déployé directus
-const port = '80';
-const url = 'http://localhost/killer';
+const port = '8012'; 
+const url = 'http://10.24.217.42:8012'; 
 const globalPath = `/api/1.1`;
 
 export const acceToken = 'UCKY3TmBGzuSt7bdnIJA04wbeqW0Ue0h';
 
-export const urlMain = 'http://localhost';
+export const urlMain = 'http://10.24.217.42:8012'; 
 
 
 export const apisUrl = `${url}${globalPath}`;
-//pour les reports
+//pour les reports 
 export const connexion = `${apisUrl}/`;
 
 
